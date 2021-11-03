c8bfd0d46d020160daf7f2684617a5786ba97a66
b0b <91906195+b0b4456@users.noreply.github.com>
2021-11-03 14:29:56 -0700

    initial build

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,5 +1,5 @@
 function foo() {
-    console.log('hello world');
+    console.log('hello world!!!');
 }
 module.exports = {
     foo
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,13 +1,5 @@
 function foo() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  console.log('hello world')
-=======
-  console.log('hello world!')
->>>>>>> f0f49fb (initial commit)
-=======
   console.log('hello world!!!')
->>>>>>> 76d346b (argh)
 }
 
 module.exports = {
