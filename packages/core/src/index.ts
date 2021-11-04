da64f88c9e1a4ba25767c39b8384cb92a9b0a38a
b0b <91906195+b0b4456@users.noreply.github.com>
2021-11-04 10:59:58 -0700

    rewrite index

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,25 @@
-function foo() {
-  console.log('hello world!!!')
-}
+import AbstractWalletManager from './abstract-wallet-manager'
+import DataMapper from './data-mapper'
+import EncryptionManager from './encryption-manager'
+// import StorageManager from './storage-manager'
+import TaskRunner from './task-runner'
 
-module.exports = {
-  foo
+import * as feeCalculatorFns from './utils/fee-calculator'
+import * as coinFormatterFns from './utils/coin-formatter'
+import * as config from './config'
+import * as types from './types'
+
+export default {
+  utils: {
+    ...feeCalculatorFns,
+    ...coinFormatterFns
+  },
+  config,
+  types,
+  AbstractWalletManager,
+  DataMapper,
+  EncryptionManager,
+  // StorageManager,
+  TaskRunner,
+
 }
