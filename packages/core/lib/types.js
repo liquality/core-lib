8c638584a6d87f8e5637aaec97acf1655826aca4
b0b <91906195+b0b4456@users.noreply.github.com>
2021-11-08 18:55:39 -0800

    fix: use importHelpers (#2)
    * importHelpers true
    
    * run build
    
    * keep built files out of github
    
    * nothing
    
    * run build on all push
    
    * fix workflow more
    
    * argh

--- packages/core/lib/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NetworkEnum = void 0;
-var NetworkEnum;
-(function (NetworkEnum) {
-    NetworkEnum["Mainnet"] = "mainnet";
-    NetworkEnum["Testnet"] = "testnet";
-})(NetworkEnum = exports.NetworkEnum || (exports.NetworkEnum = {}));
-//# sourceMappingURL=types.js.map
